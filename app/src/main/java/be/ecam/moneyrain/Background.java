2ee3ced2b0390060d0e4c873e656f457915858bc
mysherpa-aurelien <foo@foo.foo>
2016-04-13 01:12:04 +0200

    Petit exemple

--- app/src/main/java/be/ecam/moneyrain/Background.java
+++ app/src/main/java/be/ecam/moneyrain/Background.java
@@ -9,24 +9,24 @@ import android.graphics.Point;
  * Created by aurel on 12/04/2016.
  */
 public class Background extends Drawable {
-    private Point dimension;
+    //private Point dimension;
     private Point scale;
 
     Background(Resources res){
         super(res, new Point(0,0));
-        dimension = new Point(82, 75);
+        //dimension = new Point(82, 75);
     }
 
     @Override
     protected void setImage() {
-        image = BitmapFactory.decodeResource(res,R.drawable.piece);
+        image = BitmapFactory.decodeResource(res,R.drawable.background);
     }
 
     protected void draw(Canvas canvas, int width, int height){
-        scale = new Point(width/dimension.x, height/dimension.y);
+        /*scale = new Point(width/dimension.x, height/dimension.y);
         int savedState = canvas.save();
-        canvas.scale(scale.x, scale.y);
+        canvas.scale(scale.x, scale.y);*/
         draw(canvas);
-        canvas.restoreToCount(savedState);
+        //canvas.restoreToCount(savedState);
     }
 }
--- app/src/main/java/be/ecam/moneyrain/GameView.java
+++ app/src/main/java/be/ecam/moneyrain/GameView.java
@@ -15,6 +15,7 @@ public class GameView extends View {
     private Background background;
     Paint mPaint;
     private Ball mBall;
+    private Ball mBall2;
 
 
     public GameView(Context context, AttributeSet aSet) {
@@ -22,15 +23,21 @@ public class GameView extends View {
 
         mPaint = new Paint();
         background = new Background(getResources());
-        mBall = new Ball(20, 10, 5, 5, 10, BitmapFactory.decodeResource(getResources(),R.drawable.bombe));
+        mBall = new Ball(150, 150, 0, 2, 2, BitmapFactory.decodeResource(getResources(),R.drawable.piece));
+        mBall2 = new Ball(300, 200, 0, -3, -3, BitmapFactory.decodeResource(getResources(),R.drawable.billet));
     }
 
     public void next() {
-        if(mBall.x+mBall.radius > getWidth() || mBall.x-mBall.radius < 0)
+        if(mBall.x+mBall.radius > getWidth()-75 || mBall.x-mBall.radius < 0)
             mBall.verticalHit();
-        if(mBall.y+mBall.radius > getHeight() || mBall.y-mBall.radius < 0)
+        if(mBall.y+mBall.radius > getHeight()-82 || mBall.y-mBall.radius < 0)
             mBall.horizontalHit();
         mBall.next();
+        if(mBall2.x+mBall2.radius > getWidth()-98 || mBall2.x-mBall2.radius < 0)
+            mBall2.verticalHit();
+        if(mBall2.y+mBall2.radius > getHeight()-45 || mBall2.y-mBall2.radius < 0)
+            mBall2.horizontalHit();
+        mBall2.next();
         invalidate();
     }
 
@@ -38,6 +45,7 @@ public class GameView extends View {
     synchronized public void onDraw(Canvas canvas) {
         background.draw(canvas, getWidth(), getHeight());
         canvas.drawBitmap(mBall.image, mBall.x, mBall.y, null);
+        canvas.drawBitmap(mBall2.image, mBall2.x, mBall2.y, null);
     }
 
 
